1f47382119259e7b7f22126406b0bd2740d70f0c
JustMrArgus <argusprogramming@protonmail.com>
2025-09-27 01:31:17 +0300

    Add domain Order

---
+++ src/main/java/com/cosmocats/domain/Order.java
@@ -0,0 +1,18 @@
+package com.cosmocats.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    private Long id;
+    private LocalDateTime createdAt;
+    private String status;
+    private List<Long> productIds;
+}
